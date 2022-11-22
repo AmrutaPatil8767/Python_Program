26b1ebb20d69834bf351bacd47365aea5f0f7246
Patil Amruta <112855985+AmrutaPatil8767@users.noreply.github.com>
2022-11-22 09:24:48 -0600

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,4 @@
+is_odd = True
+if is_odd:
+    print("it's an odd number")
+    print('They are  divisible by 3')
---
+++ is_else.py
@@ -0,0 +1,7 @@
+is_odd = False
+if is_odd:
+    print("it's an odd number")
+    print('They are not divisible by 2')
+else:
+    print("it's an even number")
+    print('They are divisible by 2')
